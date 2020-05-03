2d00a00e8194459f192cbf1e2ee8c4973dcdb029
Gabriel Viegas da Silva <gabriel.viegas@startaideia.com.br>
2020-05-03 16:06:10 -0300

    adiciona sequelize

---
+++ database/migrations/20200503184140-create-users.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            name: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            email: {
+                allowNull: false,
+                type: Sequelize.STRING,
+            },
+            password: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: DataTypes.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: DataTypes.DATE,
+            },
+        });
+    },
+
+    down: (queryInterface) => {
+        return queryInterface.dropTable('users');
+    }
+};
