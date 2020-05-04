17d213ce4ca4b6e7eedf98d5e35373ffea811852
Gabriel Viegas da Silva <gabriel.viegas@startaideia.com.br>
2020-05-04 00:10:18 -0300

    add crud de users e inicia autenticacao

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,10 +1,77 @@
 const { user } = require('../app/models');
 
-exports.getAll = (req, res, next) => {
+exports.login = async(req, res, next) => {
 
 }
 
+exports.getAll = async(req, res, next) => {
+    try {
+        const users = await user.findAll();
+        res.status(200).json({
+            error: false,
+            data: users
+        });
+    } catch (e) {
+        res.status(400).json({
+            error: true,
+            data: [],
+            error: e
+        });
+    }
+}
+
 exports.create = async(req, res, next) => {
-    const userModel = await user.create(req.body);
-    return res.json(userModel);
+    try {
+        const userModel = await user.create(req.body);
+        return res.status(201).json({
+            error: false,
+            data: userModel
+        });
+    } catch (e) {
+        res.status(400).json({
+            error: true,
+            data: [],
+            error: e
+        });
+    }
+}
+
+exports.delete = async(req, res, next) => {
+    try {
+        await user.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        return res.status(204).json({
+            error: false,
+            message: 'Remoção sucedida'
+        });
+    } catch (e) {
+        res.status(400).json({
+            error: true,
+            data: [],
+            error: e
+        });
+    }
+}
+
+exports.update = async(req, res, next) => {
+    try {
+        const userModel = await user.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+        return res.status(200).json({
+            error: false,
+            data: userModel
+        });
+    } catch (e) {
+        res.status(400).json({
+            error: true,
+            data: [],
+            error: e
+        });
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 const userController = require('../controllers/user-controller');
 
-router.get('/users', userController.getAll);
+router.get('/', userController.getAll);
 router.post('/register', userController.create);
+router.delete('/delete/:id', userController.delete);
+router.put('/update/:id', userController.update)
+
 module.exports = router;
